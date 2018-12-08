b2a2a87fc555fb4e272d0d9028abd17a3b936bf2
georgerobbo <george.robertson3@hotmail.co.uk>
2018-12-08 01:17:17 +0000

    Add files via upload

---
+++ assignment/head.php
@@ -0,0 +1,34 @@
+<head>
+		<title>Ed's Electronics</title>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="electronics.css" />
+	</head>
+
+	<body>
+		<header>
+			<h1>Ed's Electronics</h1>
+
+
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li>Products
+					<ul>
+						<li><a href="#">TVs</a></li>
+						<li><a href="#">Computers</a></li>
+						<li><a href="#">Phones</a></li>
+						<li><a href="#">Gaming</a></li>
+					</ul>
+				</li>
+
+			</ul>
+
+			<address>
+				<p>We are open 9-5, 7 days a week. Call us on
+					<strong>01604 11111</strong>
+				</p>
+			</address>
+
+
+
+		</header>
+		<section></section>
