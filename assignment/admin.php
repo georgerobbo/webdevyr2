4c4e68ba3a35ef93d893289f75518ec933ea0296
george <george.robertson3@hotmail.co.uk>
2018-12-10 17:37:51 +0000

    adding css for the admin page

---
+++ assignment/admin.php
@@ -0,0 +1,28 @@
+<?php
+
+require 'head.php';
+
+?>
+<main>
+
+
+<div class= 'body'> 
+
+  <div class ='adminpanel'>
+    
+knfdklvncxlnlvkxcn
+    
+    
+    </div>
+
+</div>
+    
+  
+
+</main>
+
+<?php
+
+require 'foot.php';
+
+?>
