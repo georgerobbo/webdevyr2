60c45e701a586f9020c4d88a65fb3f8ba4c8f25b
george <george.robertson3@hotmail.co.uk>
2019-01-13 14:13:19 +0000

    text

--- assignment/footer.php
+++ assignment/footer.php
@@ -5,7 +5,7 @@ $results = $pdo->query('SELECT * FROM products WHERE featured ="1"');
 ?>
 
 <aside>
-
+<h1>Featured Products</h1>
     <?php	foreach ($results as $row) {
     echo '<h3>' . $row['productname'] . '</h3>';
     echo '<p>' . $row['productdesc'] . '</p>';
